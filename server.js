1ecb1442fed7c22882ad4d8e1018b1f1a510b4cf
Fabricio C Zuardi <fabricio@fabricio.org>
2015-03-18 14:48:54 -0300

    serve both classic and development website versions

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ var koa = require('koa'),
     serve = require('koa-static');
 
 const PORT = 8001;
-const ROOT = './dist/www/';
+const ROOT = './dist/classic/';
+const DEV_PORT = 8002;
+const DEV_ROOT = './dist/www/';
 
-var app = koa();
+var app = koa(),
+    devApp = koa();
 
 // Serve static files
 //------------------------------------------------------------------------------
+devApp.use(serve(DEV_ROOT));
 app.use(serve(ROOT));
 
 app.listen(PORT);
 console.log('serving at http://localhost:' + PORT);
+
+devApp.listen(DEV_PORT);
+console.log('serving at http://localhost:' + DEV_PORT);
