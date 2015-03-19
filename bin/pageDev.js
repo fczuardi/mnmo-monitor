d55379de48204cb4e7a6de338c68b8fde9952243
Fabricio C Zuardi <fabricio@fabricio.org>
2015-03-19 02:48:47 -0300

    custom background image on the dev version

--- bin/pageDev.js
+++ bin/pageDev.js
@@ -20,10 +20,26 @@ var options = {
     ]
 };
 
+var mainStyle = {
+        // width: '100%',
+        // height: '100%',
+        // position: 'absolute',
+        // display: 'table',
+        // color: 'white',
+        // fontFamily: "'Roboto Light', sans-serif",
+        // letterSpacing: 0.5,
+        // textRendering: 'geometricPrecision',
+    backgroundColor: '#070708',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center top',
+    backgroundSize: 'cover',
+    backgroundImage: 'url(./img/bg01.jpg)'
+};
+
 console.log( html(
     renderToStaticMarkup(
         createElement(Page, options, 
-            DOM.div({id:'main'},
+            DOM.div({id:'main', style: mainStyle},
                 DOM.p(null, 'carregando...')
             )
         )
