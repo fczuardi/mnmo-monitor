a0cef6ddd3da5f0b1c2f9dd2eb66bcf507ad0c09
Fabricio C Zuardi <fabricio@fabricio.org>
2015-03-23 13:47:23 -0300

    reference back-end api

---
+++ api.js
@@ -0,0 +1,20 @@
+import Router from 'koa-router';
+import URLs from './config/entrypoints.json';
+
+var router = new Router();
+
+router.get(URLs.country.list, function *(next){
+    this.body = JSON.stringify([
+        {
+            id: "0",
+            label: "USA"
+        },
+        {
+            id: "1",
+            label: "BRASIL"
+        }
+    ]);
+    yield next;
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,21 +1,32 @@
-var koa = require('koa'),
-    serve = require('koa-static');
+import koa from 'koa';
+import serve from 'koa-static';
+import APIRouter from './api';
 
 const PORT = 8001;
 const ROOT = './dist/classic/';
 const DEV_PORT = 8002;
 const DEV_ROOT = './dist/www/';
+const API_PORT = 9001;
 
 var app = koa(),
-    devApp = koa();
+    devApp = koa(),
+    api = koa();
 
 // Serve static files
 //------------------------------------------------------------------------------
 devApp.use(serve(DEV_ROOT));
 app.use(serve(ROOT));
 
+// Reference Development API
+//------------------------------------------------------------------------------
+api.use(APIRouter.routes());
+
+
 app.listen(PORT);
-console.log('serving at http://localhost:' + PORT);
+console.log('serving app at http://localhost:' + PORT);
 
 devApp.listen(DEV_PORT);
-console.log('serving at http://localhost:' + DEV_PORT);
+console.log('serving unbundled app at http://localhost:' + DEV_PORT);
+
+api.listen(API_PORT);
+console.log('serving dev API at http://localhost:' + API_PORT);
