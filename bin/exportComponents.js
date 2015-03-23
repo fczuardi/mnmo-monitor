5bd65ec43e7e603acd68a57faf5698bd8cad38b4
Fabricio C Zuardi <fabricio@fabricio.org>
2015-03-23 12:55:44 -0300

    modify shims and classic components creator to create separated actions and store files for the classic version

--- bin/exportComponents.js
+++ bin/exportComponents.js
@@ -6,19 +6,34 @@ import packageJSON from '../package.json';
 
 let globalModules = packageJSON['browserify-shim'];
 let modulesPath = join(__dirname,'../node_modules/');
+let srcPath = join(__dirname,'../src/');
+let classicPath = join(__dirname, '../dist/classic');
+
+function createUMD(entry, globalName, outFilename, isES6){
+    var b = browserify({standalone: globalName}).add(entry);
+    if (isES6) {
+        b.transform('babelify');
+    }
+    b.transform('browserify-shim')
+        .bundle()
+        .pipe(createWriteStream(outFilename, 'utf8'));
+}
 
 Object.keys(globalModules).forEach((key) => {
     if (key.indexOf('mnmo-') !== -1) {
-        let entry = join(modulesPath, key + '.js');
-        let componentName = entry.replace(/(.*)\/(.*)/,'$2');
-        let outFilename = join(__dirname, '../dist/classic/lib/js/component-' + componentName);
-        console.log(outFilename, globalModules[key].split(':')[1]);
-        browserify({
-            standalone: globalModules[key].split(':')[1]
-        })
-        .add(entry)
-        .transform('browserify-shim')
-        .bundle()
-        .pipe(createWriteStream(outFilename, 'utf8'));
+        let entry = join(modulesPath, key + '.js'),
+            componentName = entry.replace(/(.*)\/(.*)/,'$2'),
+            globalName = globalModules[key].split(':')[1],
+            outFilename = join(classicPath, 'lib/js/component-' + componentName);
+        console.log(outFilename, globalName);
+        createUMD(entry, globalName, outFilename);
+    } else if ((key.indexOf('actions') !== -1) || (key.indexOf('stores') !== -1)){
+        let globalName = globalModules[key].split(':')[1],
+            entry = join(srcPath, key + '.js'),
+            subfolder = key.split('/')[1],
+            filename = key.split('/')[2] + '.js',
+            outFilename = join(classicPath, 'js', subfolder, filename);
+        console.log(outFilename, globalName);
+        createUMD(entry, globalName, outFilename, true);
     }
 });
