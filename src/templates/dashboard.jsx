a172d266cc8dd1b8dcf823b3a3050bbd5272ac6a
Fabricio C Zuardi <fabricio@fabricio.org>
2015-03-25 18:39:37 -0300

    a stub for what might become the app drawer menu

--- src/templates/dashboard.jsx
+++ src/templates/dashboard.jsx
@@ -2,11 +2,52 @@ import React from 'react';
 
 export default (p, a) =>
 <div>
-    <p>Logado!</p>
-    <button 
-        style={({color: '#000'})}
-        onClick={a.logoutClick}
-    >
-        Logout
-    </button>
+    <ol className="list-group">
+        <ol>
+            <li className="list-group-item">
+                <label htmlFor="onOffSomething">
+                    Menu item
+                </label>
+                <input type="checkbox" id="onOffSomething" />
+            </li>
+        </ol>
+        <ol>
+            <li className="list-group-item">
+                <label htmlFor="option1">Option 1</label>
+                <input type="radio" id="option1" name="someRadioGroup"/>
+            </li>
+            <li className="list-group-item">
+                <label htmlFor="option2">Option 2</label>
+                <input type="radio" id="option2" name="someRadioGroup"/>
+            </li>
+            <li className="list-group-item">
+                <label htmlFor="option3">Option 3</label>
+                <input type="radio" id="option3" name="someRadioGroup"/>
+            </li>
+        </ol>
+        <ol>
+            <li className="list-group-item">
+                <a href="#">Menu item</a>
+            </li>
+            <li className="list-group-item">
+                <a href="#">Menu item</a>
+            </li>
+        </ol>
+        <ol>
+            <li className="list-group-item">
+                <a href="#">Menu item</a>
+            </li>
+            <li className="list-group-item">
+                <a href="#">Menu item</a>
+            </li>
+        </ol>
+        <ol>
+            <li className="list-group-item">
+                <a href="#">Menu item</a>
+            </li>
+            <li className="list-group-item">
+                <a onClick={a.logoutClick} href="#">Logout</a>
+            </li>
+        </ol>
+    </ol>
 </div>;
