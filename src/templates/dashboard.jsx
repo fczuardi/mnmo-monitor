47e305bf0808f686f0057f917bb0a9922438735c
Fabricio C Zuardi <fabricio@fabricio.org>
2015-03-25 21:23:29 -0300

    use components for lists and list items

--- src/templates/dashboard.jsx
+++ src/templates/dashboard.jsx
@@ -1,53 +1,31 @@
 import React from 'react';
+import List from 'mnmo-components/lib/themes/mnmo/list';
+import LI from 'mnmo-components/lib/li';
 
 export default (p, a) =>
 <div>
     <ol className="list-group">
-        <ol>
-            <li className="list-group-item">
-                <label htmlFor="onOffSomething">
-                    Menu item
-                </label>
-                <input type="checkbox" id="onOffSomething" />
-            </li>
-        </ol>
-        <ol>
-            <li className="list-group-item">
-                <label htmlFor="option1">Option 1</label>
-                <input type="radio" id="option1" name="someRadioGroup"/>
-            </li>
-            <li className="list-group-item">
-                <label htmlFor="option2">Option 2</label>
-                <input type="radio" id="option2" name="someRadioGroup"/>
-            </li>
-            <li className="list-group-item">
-                <label htmlFor="option3">Option 3</label>
-                <input type="radio" id="option3" name="someRadioGroup"/>
-            </li>
-        </ol>
-        <ol>
-            <li className="list-group-item">
-                <a href="#">Menu item</a>
-            </li>
-            <li className="list-group-item">
-                <a href="#">Menu item</a>
-            </li>
-        </ol>
-        <ol>
-            <li className="list-group-item">
-                <a href="#">Menu item</a>
-            </li>
-            <li className="list-group-item">
-                <a href="#">Menu item</a>
-            </li>
-        </ol>
-        <ol>
-            <li className="list-group-item">
-                <a href="#">Menu item</a>
-            </li>
-            <li className="list-group-item">
-                <a onClick={a.logoutClick} href="#">Logout</a>
-            </li>
-        </ol>
+        <List>
+            <LI type="checkbox" id="onOffSomething">
+                Menu item
+            </LI>
+        </List>
+        <List>
+            <LI type="radio" id="option1" name="someRadioGroup">Option 1</LI>
+            <LI type="radio" id="option2" name="someRadioGroup">Option 2</LI>
+            <LI type="radio" id="option3" name="someRadioGroup">Option 3</LI>
+        </List>
+        <List>
+            <LI type="link" href="#">Menu item</LI>
+            <LI type="link" href="#">Menu item</LI>
+        </List>
+        <List>
+            <LI type="link" href="#">Menu item</LI>
+            <LI type="link" href="#">Menu item</LI>
+        </List>
+        <List>
+            <LI type="link" href="#">Menu item</LI>
+            <LI type="link" onClick={a.logoutClick}>Logout</LI>
+        </List>
     </ol>
 </div>;
