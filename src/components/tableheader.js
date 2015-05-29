cea5332aebe111d81232f618d5be346b6a9102cd
Fabricio C Zuardi <fabricio@fabricio.org>
2015-05-29 14:13:51 -0300

    make columns updates trigger rerender of table headers

--- src/components/tableheader.js
+++ src/components/tableheader.js
@@ -3,7 +3,10 @@ import template from '../templates/tableheader.jsx';
 
 class TableHeader extends Component {
     shouldComponentUpdate(nextProps) {
+        let newColumns = nextProps.columns.enabled.map((column) => (column.id)).join(','),
+            oldColumns = this.props.columns.enabled.map((column) => (column.id)).join(',');
         return (
+            (newColumns !== oldColumns) ||
             (nextProps.rows.lastLoad > this.props.rows.lastLoad) ||
             (nextProps.ui.screenWidth !== this.props.ui.screenWidth)
         );
