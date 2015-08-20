715623fdcb638d7e8d7f693089e595c97e447348
Fabricio C Zuardi <fabricio@fabricio.org>
2015-08-20 19:05:49 -0300

    a line chart on top of each bar in the bar chart

--- src/templates/dashboardchart.jsx
+++ src/templates/dashboardchart.jsx
@@ -4,7 +4,17 @@ import merge from 'lodash/object/merge';
 import tableStyles from '../styles/tablestyles';
 
 const chartTopPadding = 50;
+const lineChartLength = 15; //last 15 minutes
 
+function parseData(valueString){
+    let values = valueString.split('|');
+    let parsedValues = values.map( value => parseFloat(values[0]));
+    let secondary = values[1] || null;
+    return([
+        isNaN(parsedValues[0]) ? 0 : parsedValues[0],
+        isNaN(parsedValues[1]) ? 0 : parsedValues[1]
+    ]);
+}
 export default (p) => {
     let columnColors = tableStyles(p).columnColors;
     let cellStyle = {
@@ -13,14 +23,6 @@ export default (p) => {
         width: p.columnWidth - 1,
         minWidth: p.columnWidth - 1
     };
-    let mobileCellStyle = {
-        height: p.rowHeight,
-        overflow: 'hidden',
-        position: 'relative',
-        borderRight: '1px solid #000',
-        width: 2 * p.columnWidth - 1,
-        minWidth: 2 * p.columnWidth - 1,
-    };
     let emptyCell = (
         <td key="first" style={cellStyle}></td>
     );
@@ -28,24 +30,27 @@ export default (p) => {
                 p.groups.selected.secondaryId !== -1 ?
                 p.groups.selected.secondaryId : p.groups.selected.id;
     let firstRowCells = p.rows.data[0] ? p.rows.data[0] : [];
+    //create an array of n empty arrays where n = number of columns
+    let columns = firstRowCells.map( () => ([]) ); 
+
+    let maxValue = 0; //max value in the past 15min
+    p.rows.data.forEach( (row, rowIndex) => {
+        if (rowIndex >= lineChartLength) { return; }
+        row.forEach( (cell, index) => {
+            let value = parseData(cell)[0];
+            columns[index].push(value);
+            maxValue = Math.max(maxValue, value);
+        });
+    });
     
     let textData = [],
         data = [];
     
     
     firstRowCells.forEach( (cellValue) => {
-        let values = cellValue.split('|');
-        let parsedValues = values.map( value => parseFloat(values[0]));
-        let secondary = values[1] || null;
-        textData.push(values);
-        data.push([
-            isNaN(parsedValues[0]) ? 0 : parsedValues[0],
-            isNaN(parsedValues[1]) ? 0 : parsedValues[1]
-        ]);
+        data.push(parseData(cellValue));
     });
-    let maxValue = data.reduce( (pastValue, value) => {
-        return Math.max(pastValue, value[0]);
-    }, 0);
+
     let maxPixelValue = p.chartHeight - chartTopPadding;
     let row = (
         <tr
@@ -59,7 +64,23 @@ export default (p) => {
             let values = data[key] ? data[key] : [0, 0];
             let valuePercent = values[0] / maxValue;
             let valuePixels = Math.ceil(valuePercent * maxPixelValue);
-            let textValues = textData[key] ? textData[key] : ['', ''];
+            let textValues = firstRowCells[key] ? firstRowCells[key].split('|') : ['', ''];
+            let lineChartWidth = cellStyle.width;
+            let lineChartHeight = maxPixelValue;
+            let dataHistory = columns[key];
+            let linePath = '';
+            if (dataHistory !== undefined) {
+                dataHistory.forEach( (value, rowIndex) => {
+                    let pX = (dataHistory.length - 1 - rowIndex) / (dataHistory.length - 1);
+                    let x = Math.round(pX * lineChartWidth); 
+                    let pY = value / maxValue;
+                    let y = Math.round(lineChartHeight - pY * lineChartHeight);
+                    linePath += rowIndex === 0 ? 
+                                    `M${x},${y}` :
+                                    `L${x},${y}`;
+                });
+            }
+
             return (
             <td key={key} style={cellStyle}>
                 <div
@@ -71,6 +92,22 @@ export default (p) => {
                         height: valuePixels
                     }}
                 >
+                    <svg
+                        width={lineChartWidth}
+                        height={lineChartHeight}
+                        style={{
+                            position: 'absolute',
+                            bottom: 0,
+                        }}
+                    >
+                        <path
+                            d={linePath}
+                            stroke={'white'}
+                            strokeWidth={1}
+                            fill={'none'}
+                        >
+                        </path>
+                    </svg>
                     <p
                         style={{
                             position: 'absolute',
