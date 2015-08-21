6e2d8d309c0caf9c0e444a8184f35ac354b82f56
Fabricio C Zuardi <fabricio@fabricio.org>
2015-08-21 01:53:47 -0300

    pie chart

--- src/templates/dashboardchart.jsx
+++ src/templates/dashboardchart.jsx
@@ -31,15 +31,13 @@ export default (p) => {
     let groupID = p.groups.selected === null ? '' :
                 p.groups.selected.secondaryId !== -1 ?
                 p.groups.selected.secondaryId : p.groups.selected.id;
-    let firstRowIndex = (p.rows.type === 'merged' && 
-                        p.user.autoUpdate === true) ? 1 : 0;
-    let firstRowCells = p.rows.data[firstRowIndex] ? p.rows.data[firstRowIndex] : [];
+    let firstRowCells = p.rows.data[0] ? p.rows.data[0] : [];
     //create an array of n empty arrays where n = number of columns
     let columns = firstRowCells.map( () => ([]) ); 
 
     let maxValue = 0; //max value in the past 15min
     p.rows.data.forEach( (row, rowIndex) => {
-        if (rowIndex >= lineChartLength || rowIndex <= firstRowIndex) { return; }
+        if (rowIndex >= lineChartLength) { return; }
         row.forEach( (cell, index) => {
             let value = parseData(cell)[0];
             columns[index].push(value);
@@ -56,6 +54,8 @@ export default (p) => {
     });
 
     let maxPixelValue = p.chartHeight - chartTopPadding;
+    let pieValues = [];
+    let lastPiePieceValue = 100;
     let row = (
         <tr
             style={{
@@ -66,24 +66,32 @@ export default (p) => {
         {p.columns.enabled.map( (column, key) => {
             let backgroundColor = columnColors[(key % columnColors.length)];
             let values = data[key] ? data[key] : [0, 0];
-            let valuePercent = values[0] / maxValue;
-            let valuePixels = isPercent ? 10 : Math.ceil(valuePercent * maxPixelValue);
+            let valuePercent = maxValue > 0 ? values[0] / maxValue : 0;
+            pieValues.push(values[0]);
+            lastPiePieceValue -= values[0] === 100 ? 0 : values[0];
+            let valuePixels = isPercent || maxValue === 0 ? 10 : Math.ceil(valuePercent * maxPixelValue);
+            // console.log('valuePixels', key, valuePercent, values[0], maxValue, valuePixels, valuePercent, maxPixelValue);
             let textValues = firstRowCells[key] ? firstRowCells[key].split('|') : ['', ''];
             let lineChartWidth = cellStyle.width;
             let lineChartHeight = maxPixelValue;
             let dataHistory = columns[key];
             let svgLine = null;
+            let lineDot = null;
             let mainTextValue = null;
             let secondaryTextValue = null;
             
-            if (!isPercent && dataHistory !== undefined) {
+            if (
+                !isPercent && 
+                dataHistory !== undefined &&
+                !(p.rows.type === 'merged' && p.user.autoUpdate === true)
+            ) {
                 let linePath = '';
                 dataHistory.forEach( (value, rowIndex) => {
                     let pX = (dataHistory.length - 1 - rowIndex) / (dataHistory.length - 1);
                     let x = Math.round(pX * lineChartWidth); 
                     let pY = value / maxValue;
                     let y = Math.round((lineChartHeight - 2) - pY * (lineChartHeight - 2)) + 1;
-                    linePath += `L${x},${y}`;
+                    linePath += `L${x},${y} `;
                 });
                 linePath = 'M' + linePath.substring(1);
                 // console.log('linePath', key, linePath);
@@ -106,6 +114,23 @@ export default (p) => {
                         </path>
                     </svg>
                 );
+                lineDot = (
+                    <div
+                        style={{
+                            display: isPercent ? 'none' : 'block',
+                            width: 5,
+                            height: 5,
+                            position: 'absolute',
+                            backgroundColor: '#FFFFFF',
+                            borderRadius: 5,
+                            top: -2,
+                            right: -2,
+                            zIndex: 1
+                        }}
+                    ></div>
+                );
+            }
+            if (!isPercent) {
                 mainTextValue = (
                     <p
                         style={{
@@ -149,31 +174,62 @@ export default (p) => {
                         height: valuePixels
                     }}
                 >
-                    <div
-                        style={{
-                            display: isPercent ? 'none' : 'block',
-                            width: 5,
-                            height: 5,
-                            position: 'absolute',
-                            backgroundColor: '#FFFFFF',
-                            borderRadius: 5,
-                            top: -2,
-                            right: -2,
-                            zIndex: 1
-                        }}
-                    ></div>
+                    {lineDot}
+                    {mainTextValue}
+                    {secondaryTextValue}
                 </div>
                 {svgLine}
-                {mainTextValue}
-                {secondaryTextValue}
             </td>
             );
         })}
         </tr>
     );
+    let pieChartWidth = p.ui.screenWidth;
+    let pieChartHeight = 220;
+    let centerX = pieChartWidth / 2;
+    let centerY = pieChartHeight / 2;
+    let rx = pieChartHeight / 2;
+    let ry = pieChartHeight / 2;
+    let startAngle = 0;
+    let endAngle = 0;
+    // console.log('lastPiePieceValue', lastPiePieceValue);
+    let pieChart = !isPercent ? null : (
+        <svg
+            width={pieChartWidth}
+            height={pieChartHeight}
+            style={{
+                position: 'absolute',
+                // shapeRendering: 'crispedges',
+                bottom: 30,
+            }}
+        >
+            {pieValues.map( (value, index) => {
+                if (value === 100) { return } //ignore total-kind values
+                let percent = value / 100;
+                let backgroundColor = columnColors[(index % columnColors.length)];
+                endAngle = startAngle + percent * (2 * Math.PI);
+                let x1 = Math.round(centerX + rx * Math.cos(startAngle));
+                let x2 = Math.round(centerX + rx * Math.cos(endAngle));
+                let y1 = Math.round(centerY + rx * Math.sin(startAngle));
+                let y2 = Math.round(centerY + rx * Math.sin(endAngle));
+                let piePath = `M${centerX},${centerY} L${x1},${y1} A${rx} ${ry} 0 0 1 ${x2} ${y2} z`;
+                // let piePath = `M${centerX},${centerY} L${x1},${y1} L${x2} ${y2} z`;
+                startAngle = endAngle;
+                return (
+                    <path
+                        d={piePath}
+                        strokeWidth={1}
+                        fill={backgroundColor}
+                        stroke='none'
+                    >
+                    </path>
+                );
+            })}
+        </svg>
+    );
+
     return (
         <div
-            id="table-images"
             style={{
                 width: p.ui.screenWidth,
                 overflow: 'hidden'
@@ -184,6 +240,7 @@ export default (p) => {
                     {row}
                 </tbody>
             </table>
+            {pieChart}
         </div>
     );
 };
