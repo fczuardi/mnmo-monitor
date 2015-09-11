f3286c79d63df59dc2e3ef81f538e295caf2980c
Fabricio C Zuardi <fabricio@fabricio.org>
2015-09-11 16:02:37 -0300

    brackets on the first line of the merged table

--- src/templates/rowheader.jsx
+++ src/templates/rowheader.jsx
@@ -27,21 +27,56 @@ export default (row, key, p) => {
         firstValue = row[0].split('__')[0];
         trProps.style = merge(trProps.style, tableStyles(p).separator);
     }
+    let splittedFirstValue = firstValue.split(' - ');
+    let isMerged  = (splittedFirstValue.length > 1);
+    let fontSizes = [15, 17, 25, 14, 12, 36];
+    if (isMerged && p.ui.screenHeight <= 640) {
+        fontSizes = [10, 12, 17, 9, 7, 26];
+    }
+    let titleStyle = {
+        fontSize: isMerged ? fontSizes[0] : fontSizes[1]
+    }
+    let firstTitle = isMerged ? (
+        <span
+            style={titleStyle}
+        >
+            {splittedFirstValue[0]} <br />
+            {splittedFirstValue[1]}
+        </span>
+    ): (
+        <span
+            style={titleStyle}
+        >
+            {firstValue}
+        </span>
+    );
+    let mergedIcon = (isMerged && key === 0) ? (
+        <span
+            style={{
+                position: 'absolute',
+                fontSize: fontSizes[2],
+                lineHeight: fontSizes[5] + 'px',
+                marginLeft: -10
+            }}
+        >[</span>
+    ): null;
     let firstIcon = (p.rows.headers && p.rows.headers[key][2] !== 4) ? null : 
     (
         <span 
             className="icon-attention"
             style={{
                 color: '#D1A800',
-                fontSize: 14,
+                fontSize: fontSizes[3],
                 marginRight: 5
             }}
         >
         </span>
     );
     let mainHeader = (
-        <p style={{margin: 0, fontSize: 17}}>
-            {firstIcon}{firstValue}
+        <p style={{margin: 0, fontSize: fontSizes[1]}}>
+            {firstIcon}
+            {mergedIcon}
+            {firstTitle}
         </p>
     );
     // let minuteHeader = (p.rows.type === 'detailed' && 
@@ -50,17 +85,17 @@ export default (row, key, p) => {
     // ): null;
     // let varsCount = keys(p.vars.combos).length;
     let secondHeader = isNaN(secondValue) ? (null) : (
-        <p style={{margin: 0, fontSize: 15}}>
+        <p style={{margin: 0, fontSize: fontSizes[0]}}>
             <i 
                 className={(p.user.classID !== null) ? 
                         ('header-icon-' + p.user.classID) : ''}
                 style={{
-                    fontSize: 12, 
+                    fontSize: fontSizes[4], 
                     marginRight: 3
                 }} 
             />
             <span style={{
-                    lineHeight: '15px',
+                    lineHeight: fontSizes[0] + 'px',
                     verticalAlign: 'text-top'
                 }}
             >
