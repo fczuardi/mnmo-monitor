2a57f5d556b1a82f1c9af718a0feb62d262498c5
Fabricio C Zuardi <fabricio@fabricio.org>
2015-11-13 01:43:19 -0200

    Revert "login screen logo tweaks"
    This reverts commit d62a44190027daa77830fc9c0a73399466deb5c8.

--- src/templates/login.jsx
+++ src/templates/login.jsx
@@ -43,20 +43,17 @@ export default (p, a) => {
     );
     return (
 <CenteredBox>
+    <div
+        style={{
+            position: 'relative',
+            top: 0
+        }}
+    >
+        <img src="./img/logo_login_small.png" />
+    </div>
     <form onSubmit={a.formSubmit}>
-        <FieldSet
-            legend={p.language.messages.login.welcome}
-            className="LoginFieldset"
-        >
-            <div
-                style={{
-                    position: 'absolute',
-                    top: 28,
-                    right: 28
-                }}
-            >
-                <img src="./img/logo_login_small.png" />
-            </div>
+        <FieldSet legend={p.language.messages.login.welcome}
+                    className="PPTCorners">
             <TextInput
                 value={p.user.username}
                 placeholder={p.language.messages.login.username}
