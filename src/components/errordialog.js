235c3152a6c6552f83b7318595b37ebad9953a17
Fabricio C Zuardi <fabricio@fabricio.org>
2016-01-13 00:42:31 -0200

    if the warning redirected to the password page, dont open any menu

--- src/components/errordialog.js
+++ src/components/errordialog.js
@@ -9,7 +9,9 @@ class ErrorDialog {
                 console.log('this.props.columns.enabled', this.props.columns.enabled,
                 this.props.columns.enabled.length, this.props.groups.selected);
                 userActions.errorDismissed();
-                if (this.props.groups.selected === null){
+                if (this.props.ui.screen === 'password'){
+                    //do not open menus or submenus
+                }else if (this.props.groups.selected === null){
                     this.props.flux.getActions('user').menuVisibilityToggle();
                     this.props.flux.getActions('user').openPanel('groups');
                 }else if (this.props.columns.enabled.length === 0){
