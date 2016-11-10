b0147adb2ef464c6ae48fdcc3c19e74c6e1d7276
anapribas <ana.ribas@kantaribopemedia.com>
2016-11-10 16:47:23 -0200

    task to copy secrets now needs an env var with the environment, see README

--- tasks/copy-secrets.js
+++ tasks/copy-secrets.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 require('shelljs/global')
+var urls = require('../../monitor-traditional/_dev/urls');
 
 // "copy:secret:config": "cp -R ../monitor-traditional/_dev/config ../monitor-traditional/_dev/locales ./ ",
 // "copy:secret:assets": "mkdir -p ./assets/custom/classic/ && cp ../monitor-traditional/img/*.png ../monitor-traditional/img/*.jpg ./assets/custom/classic/.",
 cp('-R', '../monitor-traditional/_dev/config', '../monitor-traditional/_dev/locales', './');
+sed('-i', 'BASE_URL', urls[process.env.NODE_ENV], './config/secret.endpoints.js');
 mkdir('-p', './assets/custom/classic/');
 cp('../monitor-traditional/img/*.png', '../monitor-traditional/img/*.jpg', './assets/custom/classic/.');
